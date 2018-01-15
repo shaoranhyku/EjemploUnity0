2606913f746701eb43b6fcd87db1b097cfc2af7d
Fran_MSI\Fran <shaoranhyku@gmail.com>
2018-01-15 19:49:29 +0100

    Añadida documentación.

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,13 +6,16 @@ public class Move : MonoBehaviour {
     
     public float forceValue;
     public float jumpValue;
+    public GameObject prefab;
     private Rigidbody rb;
     private AudioSource audiosource;
+    private GameObject capsules;
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
         audiosource = GetComponent<AudioSource>();
+        capsules = GameObject.Find("Capsules");
     }
 
     // Update is called once per frame
@@ -23,7 +26,6 @@ public class Move : MonoBehaviour {
             rb.AddForce(Vector3.up * jumpValue, ForceMode.Impulse);
             audiosource.Play();
         }
-        
     }
 
     void FixedUpdate()
@@ -33,11 +35,38 @@ public class Move : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Cubo")
+        if (collision.gameObject.CompareTag("Cubo"))
         {
             print("Colisión con cubo");
             Destroy(collision.gameObject);
         }
+        else if (collision.gameObject.CompareTag("Capsule"))
+        {
+            collision.gameObject.GetComponent<MeshRenderer>().material.color *= 2f;
+            collision.gameObject.GetComponent<AudioSource>().Play();
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Capsule"))
+        {
+            collision.gameObject.GetComponent<MeshRenderer>().material.color /= 2f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (Transform child in capsules.GetComponentInChildren<Transform>())
+        {
+            if (child.gameObject.CompareTag("Capsule"))
+            {
+                child.gameObject.GetComponent<MeshRenderer>().enabled = true;
+                child.gameObject.GetComponent<CapsuleCollider>().enabled = true;
+            }
+        }
+        Instantiate(prefab, new Vector3(Random.Range(-15f, 15f), 4f, Random.Range(-15f, 15f)), Quaternion.identity);
     }
 
 }
